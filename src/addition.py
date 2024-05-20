0763f7e68c4c1e1eb76576cd837e395376475788
Devikahdas <167571265+Devikahdas@users.noreply.github.com>
2024-05-20 13:54:13 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,7 @@
-# app.py
-# This is a test commit
-def add(a, b):
-    return a + b
-
+def add(a,b):
+    return a+b
+    
 def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(1,5) == 6
+    assert add(0,-1) == -1
+        
